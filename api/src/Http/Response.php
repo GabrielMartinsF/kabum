fa858f4bba0c9ccf13b2503ae5b58272acb4f6ea
Gabriel Martins <ubielmf@gmail.com>
2024-03-17 18:14:06 -0300

    cors

--- api/src/Http/Response.php
+++ api/src/Http/Response.php
@@ -6,9 +6,27 @@ class Response
 {
     public static function json(array $data = [], int $status = 200)
     {
-        http_response_code($status);
 
-        header("Content-Type: application/json");
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            header('Content-Type: application/json');
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400'); 
+        }
+        
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+
+                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+            
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        
+            exit(0);
+        }
+
+        http_response_code($status);
 
         echo json_encode($data);
     }

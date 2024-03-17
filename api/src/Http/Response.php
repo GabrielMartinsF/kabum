7aa451931e53edf56cae18268d834b8e29c36a8f
Gabriel Martins <ubielmf@gmail.com>
2024-03-17 18:22:25 -0300

    fix json

--- api/src/Http/Response.php
+++ api/src/Http/Response.php
@@ -6,19 +6,19 @@ class Response
 {
     public static function json(array $data = [], int $status = 200)
     {
-
+        header('Content-Type: application/json');
         if (isset($_SERVER['HTTP_ORIGIN'])) {
             header('Content-Type: application/json');
             header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
             header('Access-Control-Allow-Credentials: true');
-            header('Access-Control-Max-Age: 86400'); 
+            header('Access-Control-Max-Age: 86400');
         }
         
         if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
             
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
 
-                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+                header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
             
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
                 header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
